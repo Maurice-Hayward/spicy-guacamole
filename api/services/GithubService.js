0c49bfdc312a9768f16ba730deda8a662649912f
Maurice Hayward <mauricehayward1@gmail.com>
2017-08-20 00:08:19 -0400

    add GithubService

---
+++ api/services/GithubService.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Service = require('trails/service')
+
+const GitHubApi = require('node-github')
+
+/**
+ * @module GithubService
+ * @description get Github data
+ */
+
+module.exports = class GithubService extends Service {}
---
+++ test/unit/services/GithubService.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const assert = require('assert')
+
+describe('GithubService', () => {
+  it('should exist', () => {
+    assert(global.app.services.GithubService)
+  })
+})
