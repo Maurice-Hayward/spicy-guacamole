33d9e9d75c912008ecff2a5f718b19c9859cac9e
Maurice Hayward <mauricehayward1@gmail.com>
2017-10-06 13:34:55 -0700

    vary paraterms to fint sentiment values

--- api/controllers/GithubController.js
+++ api/controllers/GithubController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Controller = require('trails/controller')
+const _ = require('lodash')
 
 /**
  * @module GithubController
@@ -8,10 +9,13 @@ const Controller = require('trails/controller')
  */
 module.exports = class GithubController extends Controller {
   async test(request, reply) {
+    const owner = request.query.owner
+    const repo = request.query.repo
     try {
-      const issues = await this.app.services.GithubService.getAllRepoIssues('nodejs', 'node')
-      let result = this.app.services.SentimentService.getAverageSentiment(issues)
-      reply(result)
+      const issues = await this.app.services.GithubService.getAllRepoIssues(owner, repo)
+      let issueSentimentResults = this.app.services.SentimentService.getAverageSentiment(issues)
+
+      reply(_.extend({}, { owner, repo }, issueSentimentResults))
     } catch (e) {
       console.error(e)
     }
